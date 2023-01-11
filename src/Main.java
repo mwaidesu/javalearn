cc732ac66b13c0734bbe9d2ba9bdf756aa9b6c85
Mwai <machariamwai@gmail.com>
2023-01-11 14:47:15 +0300

    and behold java existed...

---
+++ src/Main.java
@@ -0,0 +1,189 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//
+//        long myLong = 5;
+//        System.out.println(myLong);
+//
+//        int a = 2;
+//        int b = 4;
+//        boolean answer = 5 >4;
+//        System.out.println(answer);
+
+//        if (a > 3){
+//            System.out.println("A is greater than 3");
+//        }
+//        else if(a < 0){
+//            System.out.println("A is less than 0");
+//        }
+//        else{
+//            System.out.println("A is either 1 or 2");
+//        }
+
+//        switch(a){
+//            case 1:
+//                System.out.println("A is 1");
+//                break;
+//            case 2:
+//                System.out.println("A is 2");
+//                break;
+//            case 3:
+//                System.out.println("A is 3");
+//                break;
+//            default:
+//                System.out.println("A is not 1, 2 or 3");
+//                break;
+//
+//        }
+
+
+//        For Loops
+//        for(int i = 1; i <= 10; i++){
+//            System.out.println(i);
+//        }
+//
+//        int i = 1;
+//        while (i <= 10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        do{
+//            System.out.println(i);
+//            i++;
+//        } while(i <=10);
+
+
+//
+//        Random random = new Random();
+//        int number = random.nextInt(20);
+//        System.out.println("Random Number is: " + number);
+
+//        int tries = 5;
+//
+//        System.out.println("Welcome to the GuessingGame.");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String name = scanner.next();
+//        System.out.println("Hello " + name);
+////
+//        System.out.println("Would you like to play a game?");
+//        System.out.println("\t1: Yes");
+//        System.out.println("\t2: No");
+//
+//        int beginAnswer = scanner.nextInt();
+//
+//        while(beginAnswer!= 1){
+//            System.out.println("Would you like to play a game?");
+//            System.out.println("\t1: Yes");
+//            System.out.println("\t2: No");
+//
+//            beginAnswer= scanner.nextInt();
+//        }
+//
+//        Random random = new Random();
+//        int x = random.nextInt(100);
+//        System.out.println("Please guess a number");
+//        int userInput = scanner.nextInt();
+//
+//        int timesTried = 0;
+//        boolean hasWon = true;
+//        boolean shouldFinish = false;
+//
+//        while(!shouldFinish){
+//            timesTried++;
+//
+//            if(timesTried < 5){
+//                if (userInput == x){
+//                    hasWon = true;
+//                    shouldFinish = true;
+//                }
+//
+//                else if (userInput > x){
+//                    System.out.println("guess lower");
+//                    userInput = scanner.nextInt();
+//
+//                }
+//                else{
+//                    System.out.println("Guess HIGHER");
+//                    userInput = scanner.nextInt();
+//                }
+//            }
+//
+//            else{
+//                shouldFinish = true;
+//            }
+//            if(hasWon){
+//                System.out.println("Congratulations.\n Times Tried: " + timesTried);
+//            }
+//            else{
+//                System.out.println("Game Over");
+//                System.out.println("The number was: " + x);
+//            }
+//        }
+
+
+//        String[] students = new String[5];
+//        students[1] = "Abdi";
+//        students[2] = "Mosh";
+//        students[0] = "Lol";
+//
+//        System.out.println(students[1]);
+//
+//        String[] employees = {"Wow", "Pow", "Dow"};
+//        System.out.println(Arrays.toString(employees));
+//
+//        int[] numbers = {1,2,3,4,5,6};
+//        for (int i=0; i<numbers.length; i++){
+//            System.out.println(numbers[i]);
+//        }
+
+//        String[] names = {"Meisan", "Sarah", "Tom", "Brad", "Brian"};
+//        int[] numbers = {134633, 252785, 737292, 362827, 282828};
+//                for (int i=0; i<names.length; i++){
+//            System.out.println(names[i]);
+//        };
+//        System.out.println("Please Enter a name: ");
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.next();
+//        for (int i =0; i<names.length; i++){
+//            if(name.equals(names[i])){
+//                System.out.println(numbers[i]);
+//            }
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
